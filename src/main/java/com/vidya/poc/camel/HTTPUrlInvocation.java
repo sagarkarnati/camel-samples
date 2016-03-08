39301c45c49218e07c891dfb01f59eb4efb71f21
vidya <vidyasagarkarnati@yahoo.com>
2016-03-08 09:23:48 -0500

    updated with mocks unit tests

--- src/main/java/com/vidya/poc/camel/HTTPUrlInvocation.java
+++ src/main/java/com/vidya/poc/camel/HTTPUrlInvocation.java
@@ -6,14 +6,12 @@ import org.apache.camel.spring.Main;
 /**
  * This camel sample explain how to invoke HTTP Rest url from camel route.
  * 
- * This reads a string from console and searches in the google.
- * 
  * @author vidya
  *
  */
 public class HTTPUrlInvocation extends RouteBuilder {
 
-	// using main since this is a sample,don't use this in the real project.
+	//Using main since this is a sample,don't use this in the real project.
 	public static void main(String[] args) throws Exception {
 
 		new Main().run(args);
@@ -22,8 +20,7 @@ public class HTTPUrlInvocation extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 
-		from("stream:in?promptMessage=What do you want to search : ").log("Your search string is :: ${body}")
+		from("direct:start").routeId("urlInvocation").log("Your search string is :: ${body}")
 				.to("https://www.google.com/search?q=${body}").log("${body}");
-	}
-
+	}	
 }
--- src/test/java/com/vidya/poc/camel/HTTPUrlInvocationTest.java
+++ src/test/java/com/vidya/poc/camel/HTTPUrlInvocationTest.java
@@ -1,38 +1,68 @@
 package com.vidya.poc.camel;
 
+import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.model.ModelCamelContext;
 import org.apache.camel.test.spring.MockEndpoints;
 import org.apache.camel.test.spring.UseAdviceWith;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@DirtiesContext(classMode=ClassMode.AFTER_EACH_TEST_METHOD)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 @MockEndpoints
 @UseAdviceWith
-@ContextConfiguration({"classpath:META-INF/spring/camel-context-test.xml"})
+@ContextConfiguration({ "classpath:META-INF/spring/camel-context-test.xml" })
 public class HTTPUrlInvocationTest {
-	
-	@Produce(uri = "mock:stream:in")
+
+	@Produce(uri = "direct:start")
 	private ProducerTemplate template;
-	
+
 	@EndpointInject(uri = "mock:https://www.google.com/search?q=${body}")
 	private MockEndpoint resultEndPoint;
 	
+	@EndpointInject(uri = "mock:advised")
+	private MockEndpoint mockResultEndPoint;
 	
+	@Autowired
+	private ModelCamelContext modelCamelContext;
+
+	// This method invokes the URL when ever tests are run.
 	@Test
-	public void test_happy_path() throws Exception
-	{
+	public void test_happy_path() throws Exception {
 		template.sendBody("Himalayas");
-		
+
 		resultEndPoint.expectedMessageCount(1);
 		resultEndPoint.assertIsSatisfied();
+	}
+
+	// This method invokes the Mock end point URL when ever tests are run.
+	@Test
+	public void test_happy_path_mock_endpoint_url() throws Exception {
+		modelCamelContext.getRouteDefinition("urlInvocation").adviceWith(modelCamelContext, new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+
+				// intercept sending to mock:foo and do something else
+				interceptSendToEndpoint("mock:https://www.google.com/search?q=${body}").skipSendToOriginalEndpoint().to("mock:advised");
+			}
+		});
+
+		template.sendBody("Himalayas");
+
+		resultEndPoint.expectedMessageCount(0);
+		mockResultEndPoint.expectedMessageCount(1);
+		resultEndPoint.assertIsSatisfied();
+		mockResultEndPoint.assertIsSatisfied();
 	}
 }
